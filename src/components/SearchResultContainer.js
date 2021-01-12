940d8c4ee84a5856b78a13cb326baca740ddef42
James Y <35856061+jmyardley@users.noreply.github.com>
2021-01-11 23:26:35 -0500

    remove search

--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import SearchForm from "./SearchForm";
 import ResultList from "./ResultList";
 import API from "../utils/API";
 
@@ -40,11 +39,7 @@ class SearchResultContainer extends Component {
   render() {
     return (
       <div>
-        <SearchForm
-          search={this.state.search}
-          handleFormSubmit={this.handleFormSubmit}
-          handleInputChange={this.handleInputChange}
-        />
+
         <ResultList results={this.state.results} />
       </div>
     );
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
 const USERSURL = "https://randomuser.me/api/?inc=gender,name,email,cell,picture,dob&results=20";
 
 // Export an object with a "search" method that searches the Giphy API for the passedquer
